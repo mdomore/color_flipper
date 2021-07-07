38ce39258424bebf63dfd8e535846ea3813ed986
Michael Moreliere <michael.moreliere@gmail.com>
2021-07-07 16:37:09 +0200

    comit changes

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const colors = ["red", "green", "#FFE4C4", "#BA55D3", "Navy"];
-const btn = document.getElementById('btn');
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const navbtn = document.getElementById('navbtn');
 const color = document.querySelector(".color");
 
-btn.addEventListener('click', function() {
-    let randomNumber = getRandomNumber();
-    document.body.style.backgroundColor = colors[randomNumber];
-    color.textContent = colors[randomNumber];
+navbtn.addEventListener('click', function() {
+    let hexColor = "#";
+    for (let i = 0; i < 6; i++) {
+        hexColor += hex[getRandomNumber()];
+    }
+    document.body.style.backgroundColor = hexColor;
 })
 
 function getRandomNumber() {
-    return Math.floor(Math.random() * colors.length);
+    return Math.floor(Math.random() * hex.length)
 }
